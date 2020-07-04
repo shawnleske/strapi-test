31f842210194d5d2df67afb8ccda118ce5d6d3bc
shawnleske <dev@shawnleske.me>
2020-07-04 07:26:54 +0200

    update database config

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
     "default": {
       "connector": "mongoose",
       "settings": {
-        "uri": "mongodb://admin:root@strapi-test-shard-00-00.bdyot.mongodb.net:27017,strapi-test-shard-00-01.bdyot.mongodb.net:27017,strapi-test-shard-00-02.bdyot.mongodb.net:27017/strapi-test?ssl=true&replicaSet=atlas-r9kbzy-shard-0&authSource=admin&retryWrites=true&w=majority",
-        "database":"strapi-test"
+        "uri": "${process.env.DATABASE_URI}",
+        "database": "${process.env.DATABASE_NAME}"
       },
       "options": {
         "ssl": true
